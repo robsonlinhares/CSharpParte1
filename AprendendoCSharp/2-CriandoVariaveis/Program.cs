b5053d9d9be077b348cbdd1e28d620cebbf5d4e2
robsonlinhares <37056453+robsonlinhares@users.noreply.github.com>
2019-01-03 13:00:25 -0200

    Primeiro projeto
    Primeiro Projeto

---
+++ AprendendoCSharp/2-CriandoVariaveis/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_CriandoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executando projeto 2 - Criando Variaveis");
+
+            int idade = 32;
+
+            Console.WriteLine(idade);
+            Console.WriteLine("Execução finalizada. Tecle enter para sair...");
+
+            Console.ReadLine();
+        }
+    }
+}
